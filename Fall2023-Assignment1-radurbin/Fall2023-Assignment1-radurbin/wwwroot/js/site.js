fc917809dc949ca5014a52287912cb4a95ec79bd
Riley Durbin <radurbin@crimson.ua.edu>
2023-09-15 14:45:37 -0500

    Moved JS and CSS to different files

--- Fall2023-Assignment1-radurbin/Fall2023-Assignment1-radurbin/wwwroot/js/site.js
+++ Fall2023-Assignment1-radurbin/Fall2023-Assignment1-radurbin/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// changes the page when you hover over the cover art
+const coverArtElement = document.getElementById("CoverArt");
+const titleElement = document.getElementById("Title");
+const originalPoster = "https://i.imgur.com/IZecVS6.jpg";
+const altPoster = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/aftersun-2022-alternate-movie-poster-miranda-jackowski.jpg";
 
-// Write your JavaScript code.
+coverArtElement.addEventListener("mouseover", function () {
+    coverArtElement.src = altPoster;
+    document.body.style.backgroundColor = "#2f441d";
+    titleElement.innerHTML = "aftersun";
+    titleElement.style.letterSpacing = "0px";
+});
+
+coverArtElement.addEventListener("mouseout", function () {
+    coverArtElement.src = originalPoster;
+    document.body.style.backgroundColor = "#56849e";
+    titleElement.innerHTML = "AFTERSUN";
+    titleElement.style.letterSpacing = "12px";
+});
